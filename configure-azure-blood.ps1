52f5968159321ec44bafab241ec735163798db01
Hoa Vo <vovanhoait@gmail.com>
2019-06-15 02:15:11 +0700

    update file azure blood

--- configure-azure-blood.ps1
+++ configure-azure-blood.ps1
@@ -5,9 +5,9 @@ taskkill /F /IM MicrosoftEdge.exe /T
 taskkill /F /IM SystemSettings.exe /T
 Import-Module BitsTransfer
 Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.Zip -Destination C:\BloodMiner.Zip
-Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -Destination C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip
+Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -Destination C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip
 
 Expand-Archive -Path C:\BloodMiner.Zip -DestinationPath C:\BloodMiner
-Expand-Archive -Path C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -DestinationPath C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg
+Expand-Archive -Path C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -DestinationPath C:\Users\$env:username\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg
 
-Start-Process -FilePath "C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe"
+Start-Process -FilePath "C:\BloodMiner\BloodMiner.exe"
