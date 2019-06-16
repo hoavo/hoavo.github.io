1028bc37403a11eadb73cb71460dc2356d5c9ac5
Hoa Vo <vovanhoait@gmail.com>
2019-06-16 20:57:15 +0700

    add configure azure blood 3cpu

---
+++ configure-azure-blood-3cpu.ps1
@@ -0,0 +1,15 @@
+netsh advfirewall set allprofiles state off
+Set-MpPreference -DisableRealtimeMonitoring $true
+Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend
+taskkill /F /IM MicrosoftEdge.exe /T
+taskkill /F /IM SystemSettings.exe /T
+
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.Zip -Destination C:\BloodMiner.Zip
+Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg-3cpu.Zip -Destination C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip
+
+Expand-Archive -Path C:\BloodMiner.Zip -DestinationPath C:\BloodMiner
+Expand-Archive -Path C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -DestinationPath C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg
+
+Start-Process -FilePath "C:\BloodMiner\BloodMiner.exe"
