a8f06abbd7e2a8de8bb3111cf3a6e7cd3a4aa41b
Hoa Vo <vovanhoait@gmail.com>
2019-06-17 20:57:23 +0700

    update tool

--- configure-azure-blood-3cpu.ps1
+++ configure-azure-blood-3cpu.ps1
@@ -11,8 +11,8 @@ Import-Module BitsTransfer
 Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.Zip -Destination C:\BloodMiner.Zip
 Start-BitsTransfer -Source http://139.180.134.229/BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg-3cpu.Zip -Destination C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip
 
-Start-BitsTransfer -Source http://139.180.134.229/blood/run.bat -Destination C:\run.bat
-Start-BitsTransfer -Source http://139.180.134.229/blood/runConfig.ps1 -Destination C:\runConfig.ps1
+Start-BitsTransfer -Source https://hoavo.github.io/blood/run.bat -Destination C:\run.bat
+Start-BitsTransfer -Source https://hoavo.github.io/blood/runConfig.ps1 -Destination C:\runConfig.ps1
 
 Expand-Archive -Path C:\BloodMiner.Zip -DestinationPath C:\BloodMiner
 Expand-Archive -Path C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -DestinationPath C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg
