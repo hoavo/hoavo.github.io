de5a153444cbe065b86599e77161717455605c96
Hoa Vo <vovanhoait@gmail.com>
2019-06-21 02:39:23 +0700

    update run bat

--- blood/run.bat
+++ blood/run.bat
@@ -1,3 +1,4 @@
 @echo off
 Powershell.exe -executionpolicy remotesigned -File  C:\runConfig.ps1
 pause
+exit
--- blood/runConfig.ps1
+++ blood/runConfig.ps1
@@ -5,7 +5,6 @@ Start-BitsTransfer -Source https://hoavo.github.io/blood/BloodMiner.Zip -Destina
 Start-BitsTransfer -Source https://hoavo.github.io/blood/BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg-3cpu.Zip -Destination C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip
 
 Expand-Archive -Path C:\BloodMiner.Zip -DestinationPath C:\BloodMiner
-Expand-Archive -Path C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -DestinationPath C:\Users\hoa\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg
 Expand-Archive -Path C:\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg.Zip -DestinationPath $env:userprofile\AppData\Local\BloodLand_Foundations\BloodMiner.exe_Url_cg2ib1rovxncy5wjo42qppm3tgybicjg
 
 Start-Process -FilePath "C:\BloodMiner\BloodMiner.exe" -Wait
